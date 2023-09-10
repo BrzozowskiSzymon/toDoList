330a12fcd717bb648c1a4cfdb2fe59e2c606bc83
Szymon Brzozowski <szymon.brzozi@gmail.com>
2023-09-10 20:00:33 +0200

    head

---
+++ README.md
@@ -0,0 +1,37 @@
+# To Do list
+The subject of the project is a To Do List. The list allows you to add new tasks to do.
+<br>Here is a working live demo -> [CLICK HERE](https://boozyboss.github.io/currencyConverter/)
+ 
+## Table of Contents
+* [Description](#description)
+* [Technologies Used](#technologies-used)
+* [Gif](#gif)
+* [Project Status](#project-status)
+* [Acknowledgements](#acknowledgements)
+* [Author](#author)
+
+
+## Description
+This website provides to do list. Can also be used on a mobile phone. It is very easy to use. Enter the task to be performed in the first gap. Then press the "Dodaj zadanie" button. The task will be available at the bottom of the page. The task can be checked or deleted.
+
+## Technologies Used
+- HTML 5
+- CSS 3
+- JavaScript
+
+## Gif
+![currencyConverter1](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWVzbmN2amp3b2kzeGtvY2p5emU0N2QwM3FueTlybzcxc3VtdzJxbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/6fnrtUYWOkkALZEwJZ/giphy.gif)
+
+## Project Status
+Project is in progress and it is the result of learning programming.
+
+## Acknowledgements
+This project was inspired by the course "Frontend Developer from the basis" by online programming school YouCode. 
+
+## Author
+Created by Szymon Brzozowski. 
+
+![Szymon](images/IMG_7526.jpg)
+
+
+  
---
+++ js/script.js
@@ -0,0 +1,90 @@
+{
+    const tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done
+        render();
+
+    }
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll('.js-remove');
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll('.js-done');
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li 
+                type="none" 
+                class="list__items">
+                <button class="js-done list__button">${task.done ? "✔" : ""}</button>
+                <span class="list__item${task.done ? " list__item--done" : ""}">${task.content}</span> 
+                <button class="js-remove list__button list__button--remove">🗑</button>
+            </li>
+            `;
+            
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString
+
+        bindEvents();
+    };
+
+
+
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const input = document.querySelector(".js-newTask");
+        const newTaskContent = input.value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+        input.value = "";
+        input.focus();
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
