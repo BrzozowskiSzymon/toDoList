61919f484d440f67a7e425d78a56a6bb0e330aa6
Szymon Brzozowski <brzozowski.szymon.98@gmail.com>
2023-10-12 00:04:50 +0200

    taskDoneCorrected

--- js/script.js
+++ js/script.js
@@ -12,7 +12,8 @@
     const toggleTaskDone = (taskIndex) => {
         tasks = [
             ...tasks.slice(0, taskIndex),
-            { ...tasks[taskIndex], done: true },
+            { ...tasks[taskIndex],
+                 done: !tasks[taskIndex].done },
             ...tasks.slice(taskIndex + 1),
         ];
         render();
